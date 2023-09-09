f5e4283bd8adeaaaf3d557c58a1dc8132544c7ae
Nabih <nabih-2001@hotmail.fr>
2023-09-09 22:41:34 +0100

    affichage avec condition and css

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -1,11 +1,17 @@
 import '../styles/App.css';
 import Cart from './Cart.js';
-import Banner from './Banner';
+import Banner from './Banner.js';
+import ShoppingList from './ShoppingList';
+import Logo from '../image/leaf+1.png';
 
 function App() {
     return (
     <div>
+       <img src = {Logo} alt='logo'/>
+
       <Banner />
+      hello
+      <ShoppingList />
       <Cart />
 
     </div>
--- todo-list/src/components/Banner.js
+++ todo-list/src/components/Banner.js
@@ -1,10 +1,15 @@
+import '../styles/Banner.css'
+
+
 function Banner() {
-    return 
-    <div className="lmj-banner">
-        <h1>
-            La maison jungle
-        </h1>
-    </div>
+    return  ( 
+            <div className="lmj-banner"> 
+            Je suis le banner  </div>
+            )
+
+    
+    
+    
 }
 
 export default Banner
