6891c913daacaa044fbf1c847bc588c3f6f23bb1
Nabih <nabih-2001@hotmail.fr>
2023-09-09 14:45:56 +0100

    Creation componant

---
+++ todo-list/src/components/App.js
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+import Cart from './Cart.js';
+import Banner from './Banner';
+
+function App() {
+    return (
+    <div>
+      <Banner />
+      <Cart />
+
+    </div>
+    )
+}
+
+export default App
+
